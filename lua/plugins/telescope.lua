bfc9809702732318143030b6de3531aa8fe366d0
Luke Brannagan <luke-brannagan@hotmail.com>
2025-07-06 06:24:37 +0100

    fix: telescope now doesn't select the wrong option and doesn't ff through node-mods

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,10 +1,27 @@
-return { 
-   "nvim-telescope/telescope.nvim", 
-   opts = function(_, opts) 
-     local actions = require "telescope.actions" 
+return {
+  "nvim-telescope/telescope.nvim",
+  opts = function(_, opts)
+    local actions = require "telescope.actions"
 
+    -- Ensure `opts.defaults` exists
+    opts.defaults = opts.defaults or {}
+
+    -- Add file ignore patterns
     opts.defaults.file_ignore_patterns = {
-      "node_modules"
+      "node_modules",
     }
-   end 
- }
+
+    -- Add or override key mappings
+    opts.defaults.mappings = {
+      i = {
+        ["<CR>"] = actions.select_default, -- Enter selects current entry in insert mode
+        ["<C-q>"] = actions.smart_send_to_qflist + actions.open_qflist, -- optional
+      },
+      n = {
+        ["<CR>"] = actions.select_default, -- Enter in normal mode
+      },
+    }
+
+    return opts
+  end,
+}
--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -95,4 +95,19 @@ return {
     "sphamba/smear-cursor.nvim",
     opts = {},
   },
+  {
+    "nvim-neotest/neotest",
+    dependencies = {
+      "marilari88/neotest-vitest",
+      "nvim-neotest/nvim-nio",
+      "nvim-lua/plenary.nvim",
+      "antoinemadec/FixCursorHold.nvim",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    opts = {
+      adapters = {
+        ["neotest-vitest"] = {},
+      },
+    },
+  },
 }
